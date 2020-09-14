e3936e7bc19df7b7fff12afc9ace467139a2134a
Vadym Bytsiv <Vadym_Bytsiv@epam.com>
2020-09-14 18:10:30 +0200

    Minor css.

--- src/AddMovie/AddMovie.js
+++ src/AddMovie/AddMovie.js
@@ -34,19 +34,19 @@ class AddMovie extends React.Component {
             <div className="addMovie">
                 <span id="addMovieSpan">ADD MOVIE</span>
                 <span>TITLE</span>
-                <input type="text" className="textBox" onChange={e => this.setState({title: e.target.value})} value={this.state.title} />
+                <input type="text" onChange={e => this.setState({title: e.target.value})} value={this.state.title} />
                 <span>RELEASE DATE</span>
-                <input type="text" className="textBox" onChange={e => this.setState({date: e.target.value})}  value={this.state.date} />
+                <input type="text" onChange={e => this.setState({date: e.target.value})}  value={this.state.date} />
                 <span>MOVIE URL</span>
-                <input type="text" className="textBox" onChange={e => this.setState({url: e.target.value})}  value={this.state.url} />
+                <input type="text" onChange={e => this.setState({url: e.target.value})}  value={this.state.url} />
                 <span>GENRE</span>
                 <select onChange={e => this.setState({genreId: e.target.value})}>
                     {this.props.genres.map(x => (<option value={x.id}>{x.name}</option>))}
                 </select>
                 <span>OVERVIEW</span>
-                <input type="text" className="textBox" onChange={e => this.setState({overview: e.target.value})}  value={this.state.overview} />
+                <input type="text" onChange={e => this.setState({overview: e.target.value})}  value={this.state.overview} />
                 <span>RUNTIME</span>
-                <input type="text" className="textBox" onChange={e => this.setState({runtime: e.target.value})}  value={this.state.runtime} />
+                <input type="text" onChange={e => this.setState({runtime: e.target.value})}  value={this.state.runtime} />
                 <div className="buttons">
                     <button className="resetButton" onClick={this.setInitialState}>RESET</button>
                     <button className="submitButton" onClick={() => this.props.handleMovieAdded(
